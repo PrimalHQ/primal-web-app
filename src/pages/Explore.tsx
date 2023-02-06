effa5e7242cc256bdc5aaa7a49cfd96d42a7e753
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-02-06 16:09:22 +0100

    Home and Explore pages

---
+++ src/pages/Explore.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+const Explore: Component = () => {
+
+    return (
+      <div>
+        Explore Page
+      </div>
+    )
+}
+
+export default Explore;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,63 @@
+import { Component, createSignal, For, Match, onMount, Switch } from 'solid-js';
+import Post from '../components/Post/Post';
+import styles from './Home.module.scss';
+import { useFeedContext } from '../contexts/FeedContext';
+import { Portal } from 'solid-js/web';
+import FeedSelect from '../components/FeedSelect/FeedSelect';
+
+const Home: Component = () => {
+
+  const context = useFeedContext();
+
+  const [mounted, setMounted] = createSignal(false);
+
+  onMount(() => {
+    // Temporary fix for Portal rendering on initial load.
+    setMounted(true);
+  });
+
+  return (
+    <div class={styles.homeContent}>
+      <Switch>
+        <Match when={mounted()}>
+          <Portal 
+            mount={document.getElementById("subheader_center") as Node}
+          >
+            <FeedSelect />
+          </Portal>
+
+          <Portal 
+            mount={document.getElementById("right_sidebar") as Node}
+          >
+            <h4>Trending</h4>
+            <aside>
+              <ul>
+                <li>
+                  <div>
+                    <span class={styles.trendAuthor}>Snowden</span>
+                    <span class={styles.trendTime}>5 hours ago</span>
+                  </div>
+                  <div class={styles.trendContent}>
+                    The problem is that most major states are pressuring corporations to limit speech...
+                  </div>
+                </li>
+              </ul>
+            </aside>
+          </Portal>
+        </Match>
+      </Switch>
+      
+      <For each={context?.data?.posts} >
+        {(post) => {
+          console.log('POST: ', post);
+          return <Post 
+            post={post}
+          />
+        }
+        }
+      </For>
+    </div>
+  )
+}
+
+export default Home;
