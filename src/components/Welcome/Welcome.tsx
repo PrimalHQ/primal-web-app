45b4b3b8fc0a8f030d6e55e356b0b0e8ae99fea3
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-02-06 16:06:54 +0100

    Welcome component

---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,19 @@
+import { Component } from 'solid-js';
+import Avatar from '../Avatar/Avatar';
+import ThemeToggle from '../ThemeToggle/ThemeToggle';
+
+import styles from './Welcome.module.scss';
+import jack from '/icons/jack.png';
+
+const Welcome: Component = () => {
+  return (
+    <div class={styles.welcome}>
+      <Avatar src={jack} />
+      <div>
+        <ThemeToggle />
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
