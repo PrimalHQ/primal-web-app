eafc691a483ed1cd01b99225160a9c314b1d750c
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-02-17 13:26:45 +0100

    Logo links to home page

--- src/components/Branding/Branding.tsx
+++ src/components/Branding/Branding.tsx
@@ -2,23 +2,26 @@ import { Component, Match, Show, Switch } from 'solid-js';
 
 import styles from './Branding.module.scss';
 import logo from '../../assets/icons/logo.svg';
+import { A } from '@solidjs/router';
 
 const Branding: Component<{ small: boolean }> = (props) => {
 
     return (
-      <Show
-        when={!props.small}
-        fallback={
-          <div class={styles.brandingSmall}>
+      <A href="/home" class={styles.logoLink}>
+        <Show
+          when={!props.small}
+          fallback={
+            <div class={styles.brandingSmall}>
+              <img src={logo} alt="logo" />
+            </div>
+          }
+        >
+          <div class={styles.branding}>
             <img src={logo} alt="logo" />
+            <span>primal</span>
           </div>
-        }
-      >
-        <div class={styles.branding}>
-          <img src={logo} alt="logo" />
-          <span>primal</span>
-        </div>
-      </Show>
+        </Show>
+      </A>
     )
 }
 
