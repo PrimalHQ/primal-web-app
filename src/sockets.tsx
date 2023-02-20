a8f0854e13c1dbe56e3a3330594bc6c595b3370c
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-02-20 21:32:50 +0100

    Use cache6

--- src/sockets.tsx
+++ src/sockets.tsx
@@ -16,7 +16,7 @@ const onClose = () => {
 
 export const connect = () => {
   if (isNotConnected()) {
-    setSocket(new WebSocket('wss://dev.primal.net/cache5'));
+    setSocket(new WebSocket('wss://dev.primal.net/cache6'));
 
     socket()?.addEventListener('open', onOpen);
     socket()?.addEventListener('close', onClose);
