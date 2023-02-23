e158b3946c4310e0a82bfa6064c09e1eb783416e
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-02-23 12:16:22 +0100

    Move verified icon in Post's user info, 2px down

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -44,71 +44,71 @@ const Post: Component<{ post: PrimalNote }> = (props) => {
 
   // const [time] = createSignal(date(props.post?.post.created_at));
 
-    return (
-      <A class={styles.postLink} href={`/thread/${props.post?.post.id}`}>
-        <div class={styles.post}>
-          <div class={styles.avatar} title={props.post?.user?.name}>
-            <Avatar
-              src={props.post?.user?.picture}
-              size="md"
-              verified={props.post?.user?.nip05}
-            />
-            <div class={styles.avatarName}>{props.post?.user?.name}</div>
-          </div>
-          <div class={styles.content}>
-            <div class={styles.header}>
-              <span class={styles.postInfo}>
-                <span class={styles.userInfo}>
-                  <span class={styles.userName}>
-                    {props.post?.user?.name}
-                  </span>
-                  <Switch>
-                    <Match when={props.post?.user?.nip05}>
-                      <span class={styles.verifiedIcon} />
-                      <span
-                        class={styles.verifiedBy}
-                        title={props.post?.user?.nip05}
-                      >
-                        {trimVerification(props.post?.user?.nip05)}
-                      </span>
-                    </Match>
-                  </Switch>
+  return (
+    <A class={styles.postLink} href={`/thread/${props.post?.post.id}`}>
+      <div class={styles.post}>
+        <div class={styles.avatar} title={props.post?.user?.name}>
+          <Avatar
+            src={props.post?.user?.picture}
+            size="md"
+            verified={props.post?.user?.nip05}
+          />
+          <div class={styles.avatarName}>{props.post?.user?.name}</div>
+        </div>
+        <div class={styles.content}>
+          <div class={styles.header}>
+            <span class={styles.postInfo}>
+              <span class={styles.userInfo}>
+                <span class={styles.userName}>
+                  {props.post?.user?.name}
                 </span>
-                <span
-                  class={styles.time}
-                  title={date(props.post?.post?.created_at).date.toLocaleString()}
-                >
-                  {date(props.post?.post?.created_at).label}
-                </span>
+                <Switch>
+                  <Match when={props.post?.user?.nip05}>
+                    <span class={styles.verifiedIcon} />
+                    <span
+                      class={styles.verifiedBy}
+                      title={props.post?.user?.nip05}
+                    >
+                      {trimVerification(props.post?.user?.nip05)}
+                    </span>
+                  </Match>
+                </Switch>
               </span>
-              <div class={styles.contextMenu}>...</div>
-            </div>
+              <span
+                class={styles.time}
+                title={date(props.post?.post?.created_at).date.toLocaleString()}
+              >
+                {date(props.post?.post?.created_at).label}
+              </span>
+            </span>
+            <div class={styles.contextMenu}>...</div>
+          </div>
 
-            <div class={styles.message} innerHTML={urlify(props.post?.post?.content)}>
-            </div>
+          <div class={styles.message} innerHTML={urlify(props.post?.post?.content)}>
+          </div>
 
-            <div class={styles.footer}>
-              <div class={styles.stat}>
-                <div class={styles.replyIcon}></div>
-                <div class={styles.statNumber}>{props.post?.post?.replies}</div>
-              </div>
-              <div class={styles.stat}>
-                <div class={styles.likeIcon}></div>
-                <div class={styles.statNumber}>{props.post?.post?.likes}</div>
-              </div>
-              <div class={styles.stat}>
-                <div class={styles.repostIcon}></div>
-                <div class={styles.statNumber}>{props.post?.post?.mentions}</div>
-              </div>
-              <div class={styles.stat}>
-                <div class={styles.zapIcon}></div>
-                <div class={styles.statNumber}>{props.post?.post?.satszapped}</div>
-              </div>
+          <div class={styles.footer}>
+            <div class={styles.stat}>
+              <div class={styles.replyIcon}></div>
+              <div class={styles.statNumber}>{props.post?.post?.replies}</div>
+            </div>
+            <div class={styles.stat}>
+              <div class={styles.likeIcon}></div>
+              <div class={styles.statNumber}>{props.post?.post?.likes}</div>
+            </div>
+            <div class={styles.stat}>
+              <div class={styles.repostIcon}></div>
+              <div class={styles.statNumber}>{props.post?.post?.mentions}</div>
+            </div>
+            <div class={styles.stat}>
+              <div class={styles.zapIcon}></div>
+              <div class={styles.statNumber}>{props.post?.post?.satszapped}</div>
             </div>
           </div>
         </div>
-      </A>
-    )
+      </div>
+    </A>
+  )
 }
 
 export default Post;
