4b8bb07ea378c0342664645346042edb988c633f
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-04-03 17:15:00 +0200

    Use translations in sidebar nav menu

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,3 +1,4 @@
+import { useIntl } from '@cookbook/solid-intl';
 import { Component, For, Show } from 'solid-js';
 import { useAccountContext } from '../../contexts/AccountContext';
 import NavLink from '../NavLink/NavLink';
@@ -7,15 +8,72 @@ import styles from './NavMenu.module.scss';
 
 const NavMenu: Component = () => {
   const account = useAccountContext();
+  const intl = useIntl();
 
   const links = [
-    { to: '/home', label: 'Home', icon: 'homeIcon' },
-    { to: '/explore', label: 'Explore', icon: 'exploreIcon' },
-    { to: '/messages', label: 'Messages', icon: 'messagesIcon' },
-    { to: '/notifications', label: 'Notifications', icon: 'notificationsIcon' },
-    { to: '/downloads', label: 'Downloads', icon: 'downloadIcon' },
-    { to: '/settings', label: 'Settings', icon: 'settingsIcon' },
-    { to: '/help', label: 'Help', icon: 'helpIcon' },
+    {
+      to: '/home',
+      label: intl.formatMessage({
+        id: 'navbar.home',
+        defaultMessage: 'Home',
+        description: 'Label for the nav bar item link to Home page',
+      }),
+      icon: 'homeIcon',
+    },
+    {
+      to: '/explore',
+      label: intl.formatMessage({
+        id: 'navbar.explore',
+        defaultMessage: 'Explore',
+        description: 'Label for the nav bar item link to Explore page',
+      }),
+      icon: 'exploreIcon',
+    },
+    {
+      to: '/messages',
+      label: intl.formatMessage({
+        id: 'navbar.messages',
+        defaultMessage: 'Messages',
+        description: 'Label for the nav bar item link to Messages page',
+      }),
+      icon: 'messagesIcon',
+    },
+    {
+      to: '/notifications',
+      label: intl.formatMessage({
+        id: 'navbar.notifications',
+        defaultMessage: 'Notifications',
+        description: 'Label for the nav bar item link to Notifications page',
+      }),
+      icon: 'notificationsIcon',
+    },
+    {
+      to: '/downloads',
+      label: intl.formatMessage({
+        id: 'navbar.downloads',
+        defaultMessage: 'Downloads',
+        description: 'Label for the nav bar item link to Downloads page',
+      }),
+      icon: 'downloadIcon',
+    },
+    {
+      to: '/settings',
+      label: intl.formatMessage({
+        id: 'navbar.settings',
+        defaultMessage: 'Settings',
+        description: 'Label for the nav bar item link to Settings page',
+      }),
+      icon: 'settingsIcon',
+    },
+    {
+      to: '/help',
+      label: intl.formatMessage({
+        id: 'navbar.help',
+        defaultMessage: 'Help',
+        description: 'Label for the nav bar item link to Help page',
+      }),
+      icon: 'helpIcon',
+    },
   ];
 
   return (
