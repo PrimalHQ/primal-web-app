4ea945d94c510065758436144287a58831c675a1
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-04-19 17:41:01 +0200

    Quick fix icon imports

--- src/components/Notifications/NotificationItem.tsx
+++ src/components/Notifications/NotificationItem.tsx
@@ -1,7 +1,7 @@
 import { useIntl } from '@cookbook/solid-intl';
 import { A } from '@solidjs/router';
 import { Component, createEffect, createMemo, createSignal, For, lazy, Show } from 'solid-js';
-import { NotificationType, notificationTypeTranslations, typeIcons } from '../../constants';
+import { NotificationType, notificationTypeTranslations } from '../../constants';
 import { trimVerification } from '../../lib/profile';
 import { truncateNpub } from '../../stores/profile';
 import { PrimalNote, PrimalNotifUser, PrimalUser } from '../../types/primal';
@@ -9,7 +9,50 @@ import Avatar from '../Avatar/Avatar';
 
 import styles from './NotificationItem.module.scss';
 
-const iconRoot = '../../assets/icons/notifications';
+import userFollow from '../../assets/icons/notifications/user_followed.svg';
+import userUnFollow from '../../assets/icons/notifications/user_unfollowed.svg';
+
+import postZapped from '../../assets/icons/notifications/post_zapped.svg';
+import postLiked from '../../assets/icons/notifications/post_liked.svg';
+import postReposted from '../../assets/icons/notifications/post_reposted.svg';
+import postReplied from '../../assets/icons/notifications/post_replied.svg';
+
+import mention from '../../assets/icons/notifications/mention.svg';
+import mentionedPost from '../../assets/icons/notifications/mentioned_post.svg';
+
+import mentionZapped from '../../assets/icons/notifications/mention_zapped.svg';
+import mentionLiked from '../../assets/icons/notifications/mention_liked.svg';
+import mentionReposted from '../../assets/icons/notifications/mention_reposted.svg';
+import mentionReplied from '../../assets/icons/notifications/mention_replied.svg';
+
+import mentionedPostZapped from '../../assets/icons/notifications/mentioned_post_zapped.svg';
+import mentionedPostLiked from '../../assets/icons/notifications/mentioned_post_liked.svg';
+import mentionedPostReposted from '../../assets/icons/notifications/mentioned_post_reposted.svg';
+import mentionedPostReplied from '../../assets/icons/notifications/mentioned_post_replied.svg';
+
+const typeIcons: Record<string, string> = {
+  [NotificationType.NEW_USER_FOLLOWED_YOU]: userFollow,
+  [NotificationType.USER_UNFOLLOWED_YOU]: userUnFollow,
+
+  [NotificationType.YOUR_POST_WAS_ZAPPED]: postZapped,
+  [NotificationType.YOUR_POST_WAS_LIKED]: postLiked,
+  [NotificationType.YOUR_POST_WAS_REPOSTED]: postReposted,
+  [NotificationType.YOUR_POST_WAS_REPLIED_TO]: postReplied,
+
+  [NotificationType.YOU_WERE_MENTIONED_IN_POST]: mention,
+  [NotificationType.YOUR_POST_WAS_MENTIONED_IN_POST]: mentionedPost,
+
+  [NotificationType.POST_YOU_WERE_MENTIONED_IN_WAS_ZAPPED]: mentionZapped,
+  [NotificationType.POST_YOU_WERE_MENTIONED_IN_WAS_LIKED]: mentionLiked,
+  [NotificationType.POST_YOU_WERE_MENTIONED_IN_WAS_REPOSTED]: mentionReposted,
+  [NotificationType.POST_YOU_WERE_MENTIONED_IN_WAS_REPLIED_TO]: mentionReplied,
+
+  [NotificationType.POST_YOUR_POST_WAS_MENTIONED_IN_WAS_ZAPPED]: mentionedPostZapped,
+  [NotificationType.POST_YOUR_POST_WAS_MENTIONED_IN_WAS_LIKED]: mentionedPostLiked,
+  [NotificationType.POST_YOUR_POST_WAS_MENTIONED_IN_WAS_REPOSTED]:mentionedPostReposted,
+  [NotificationType.POST_YOUR_POST_WAS_MENTIONED_IN_WAS_REPLIED_TO]: mentionedPostReplied,
+
+}
 
 type NotificationItemProps = {
   type: NotificationType,
@@ -90,10 +133,7 @@ const NotificationItem: Component<NotificationItemProps> = (props) => {
   }
 
   createEffect(() => {
-    import(/* @vite-ignore */`${iconRoot}/${typeIcons[props.type]}`).
-      then((icon) => {
-        setTypeIcon(icon.default);
-      });
+    setTypeIcon(typeIcons[props.type])
   });
 
   return (
