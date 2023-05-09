0fc339303c67114dee0f69c9eb56c4a4327cead5
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-05-09 12:49:55 +0200

    Add 'copy note link' option in the context

--- src/components/Note/NoteHeader/NoteHeader.tsx
+++ src/components/Note/NoteHeader/NoteHeader.tsx
@@ -1,12 +1,19 @@
-import { Component, createMemo, Match, Show, Switch } from 'solid-js';
+import { Component, createEffect, createMemo, createSignal, Show } from 'solid-js';
 import { PrimalNote } from '../../../types/primal';
 
 import styles from './NoteHeader.module.scss';
 import { date } from '../../../lib/dates';
 import { trimVerification } from '../../../lib/profile';
 import { truncateNpub } from '../../../stores/profile';
+import { useIntl } from '@cookbook/solid-intl';
+import { useToastContext } from '../../Toaster/Toaster';
 
 const NoteHeader: Component<{ note: PrimalNote}> = (props) => {
+
+  const intl = useIntl();
+  const toaster = useToastContext();
+
+  const [showContext, setContext] = createSignal(false);
 
   const authorName = () => {
     return props.note.user?.displayName ||
@@ -16,6 +23,39 @@ const NoteHeader: Component<{ note: PrimalNote}> = (props) => {
 
   const verification = createMemo(() => {
     return trimVerification(props.note.user?.nip05);
+  });
+
+  const openContextMenu = (e: MouseEvent) => {
+    e.preventDefault();
+    setContext(true);
+  };
+
+  const copyNoteLink = (e: MouseEvent) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(`nostr:${props.note.post.noteId}`);
+    setContext(false);
+    toaster?.sendSuccess(intl.formatMessage({
+      id: 'note.contextMenu.copyFeedback',
+      defaultMessage: 'Note\'s nostr link copied',
+      description: 'Confirmation message that the note\'s link has been copied',
+    }));
+  };
+
+  const onClickOutside = (e: MouseEvent) => {
+    if (
+      !document?.getElementById(`note_context_${props.note.post.id}`)?.contains(e.target as Node)
+    ) {
+      setContext(false);
+    }
+  }
+
+  createEffect(() => {
+    if (showContext()) {
+      document.addEventListener('click', onClickOutside);
+    }
+    else {
+      document.removeEventListener('click', onClickOutside);
+    }
   });
 
   return (
@@ -50,7 +90,24 @@ const NoteHeader: Component<{ note: PrimalNote}> = (props) => {
         </span>
       </span>
       <div class={styles.contextMenu}>
-        <div class={styles.contextIcon}></div>
+        <button onClick={openContextMenu} class={styles.contextIcon}></button>
+        <Show when={showContext()}>
+          <div
+            id={`note_context_${props.note.post.id}`}
+            class={styles.contextMenuOptions}
+          >
+            <button
+              onClick={copyNoteLink}
+              class={styles.contextOption}
+            >
+              {intl.formatMessage({
+                id: 'note.contextMenu.copyLink',
+                defaultMessage: 'Copy note link',
+                description: 'Label for the copy note link context menu item',
+              })}
+            </button>
+          </div>
+        </Show>
       </div>
     </div>
   )
