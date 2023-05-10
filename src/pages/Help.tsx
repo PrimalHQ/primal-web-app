023753c7e952334dbff14aeec31ceef2b90786e9
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-05-10 16:19:13 +0200

    Add Loader to Help page

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,10 +1,14 @@
 import { Component } from 'solid-js';
+import Loader from '../components/Loader/Loader';
 import MissingPage from '../components/MissingPage/MissingPage';
 
 
 const Help: Component = () => {
   return (
-    <MissingPage title="help" />
+    <>
+      <MissingPage title="help" />
+      <Loader />
+    </>
   );
 }
 
