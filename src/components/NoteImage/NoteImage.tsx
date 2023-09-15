adad2473ce4e53a0c5508f1bbce9aafb9f1e5856
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-09-15 15:00:18 +0200

    Remove image zoom

--- src/components/NoteImage/NoteImage.tsx
+++ src/components/NoteImage/NoteImage.tsx
@@ -1,7 +1,5 @@
-import { Component, JSX, onCleanup, onMount } from "solid-js";
+import { Component, JSX } from "solid-js";
 import styles from "./NoteImage.module.scss";
-import mediumZoom from "medium-zoom";
-import type { Zoom } from 'medium-zoom';
 // @ts-ignore Bad types in nostr-tools
 import { generatePrivateKey } from "nostr-tools";
 
@@ -12,45 +10,7 @@ const NoteImage: Component<{
 }> = (props) => {
   const imgId = generatePrivateKey();
 
-  const imgRef = () => {
-    return document.getElementById(imgId)
-  };
-
-  let zoomRef: Zoom | undefined;
-
   const klass = () => `${styles.noteImage} ${props.isDev ? 'redBorder' : ''}`;
-
-  const doZoom = (e: MouseEvent) => {
-    if (!e.target || (e.target as HTMLImageElement).id !== imgId) {
-      return;
-    }
-
-    zoomRef?.open();
-  };
-
-  const getZoom = () => {
-    const iRef = imgRef();
-    if (zoomRef || !iRef) {
-      return zoomRef;
-    }
-
-   zoomRef = mediumZoom(iRef, {
-        background: "var(--background-site)",
-    });
-
-    zoomRef.attach(iRef);
-  }
-
-  onMount(() => {
-    getZoom();
-    document.addEventListener('click', doZoom)
-  });
-
-  onCleanup(() => {
-    const iRef = imgRef();
-    iRef && zoomRef && zoomRef.detach(iRef);
-    document.removeEventListener('click', doZoom)
-  });
 
   return <img id={imgId} src={props.src} class={klass()} onerror={props.onError} />;
 }
