2124f3b6630b89c02b70d0bf3257dd3e74aa92a6
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-09-26 16:15:44 +0200

    Add android v0.16.8

--- src/constants.ts
+++ src/constants.ts
@@ -283,8 +283,8 @@ export const today = (new Date()).getTime();
 export const iosRD = (new Date('09/07/2023')).getTime();
 export const iosVersion = '0.28.2';
 
-export const andRD = (new Date('08/30/2023')).getTime();
-export const andVersion = '0.15.2';
+export const andRD = (new Date('09/26/2023')).getTime();
+export const andVersion = '0.16.8';
 
 export const defaultZapAmount = 10;
 
