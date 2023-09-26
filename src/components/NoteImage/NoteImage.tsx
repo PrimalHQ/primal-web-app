dc69685e2fea8829b85c605d7a0722a3e81eb7b8
Bojan Mojsilovic <bmojsilovic@itekako.com>
2023-09-26 17:33:14 +0200

    Revert "Revert "Remove image zoom""
    This reverts commit 99c7011d21396c1bb180312f04491c897f41e5f3.

--- src/components/NoteImage/NoteImage.tsx
+++ src/components/NoteImage/NoteImage.tsx
@@ -1,7 +1,5 @@
-import { Component, createEffect, JSX, onCleanup, onMount } from "solid-js";
+import { Component, JSX } from "solid-js";
 import styles from "./NoteImage.module.scss";
-import mediumZoom from "medium-zoom";
-import type { Zoom } from 'medium-zoom';
 // @ts-ignore Bad types in nostr-tools
 import { generatePrivateKey } from "nostr-tools";
 
@@ -12,51 +10,9 @@ const NoteImage: Component<{
 }> = (props) => {
   const imgId = generatePrivateKey();
 
-  const imgRef = () => {
-    return document.getElementById(imgId)
-  };
-
-  let zoomRef: Zoom | undefined;
-
   const klass = () => `${styles.noteImage} ${props.isDev ? 'redBorder' : ''}`;
 
-  const doZoom = (e: MouseEvent) => {
-    if (!e.target || (e.target as HTMLImageElement).id !== imgId) {
-      return;
-    }
-    e.preventDefault();
-    e.stopPropagation();
-
-    zoomRef?.open();
-  };
-
-  const getZoom = () => {
-    const iRef = imgRef();
-    if (zoomRef || !iRef) {
-      return zoomRef;
-    }
-
-   zoomRef = mediumZoom(iRef, {
-        background: "var(--background-site)",
-    });
-
-    zoomRef.attach(iRef);
-  }
-
-  onMount(() => {
-    getZoom();
-  });
-
-  onCleanup(() => {
-    const iRef = imgRef();
-    iRef && zoomRef && zoomRef.detach(iRef);
-  });
-
-  return (
-    <div>
-      <img id={imgId} src={props.src} class={klass()} onerror={props.onError} onClick={doZoom} />
-    </div>
-  );
+  return <img id={imgId} src={props.src} class={klass()} onerror={props.onError} />;
 }
 
 export default NoteImage;
