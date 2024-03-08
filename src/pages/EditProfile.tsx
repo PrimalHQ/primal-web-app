ea79b780656bafcc4d9784504b7538c458eb7bda
Bojan Mojsilovic <bmojsilovic@itekako.com>
2024-03-08 15:17:21 +0100

    Import edited profile after update

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -22,6 +22,8 @@ import PageTitle from '../components/PageTitle/PageTitle';
 import ButtonPrimary from '../components/Buttons/ButtonPrimary';
 import ButtonSecondary from '../components/Buttons/ButtonSecondary';
 import Uploader from '../components/Uploader/Uploader';
+import { triggerImportEvents } from '../lib/notes';
+import { APP_ID } from '../App';
 
 type AutoSizedTextArea = HTMLTextAreaElement & { _baseScrollHeight: number };
 
@@ -222,9 +224,10 @@ const EditProfile: Component = () => {
 
     const oldProfile = profile?.userProfile || {};
 
-    const { success } = await sendProfile({ ...oldProfile, ...metadata}, account.relays, account.relaySettings);
+    const { success, note } = await sendProfile({ ...oldProfile, ...metadata}, account.relays, account.relaySettings);
 
     if (success) {
+      note && triggerImportEvents([note], `import_profile_${APP_ID}`);
       toast?.sendSuccess(intl.formatMessage(tToast.updateProfileSuccess))
       return false;
     }
