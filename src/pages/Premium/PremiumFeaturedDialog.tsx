a115f33d85515466128f0a644d4f81d2aa02e7e9
Bojan Mojsilovic <bmojsilovic@itekako.com>
2024-11-19 18:20:36 +0100

    Premium: FAQ and Features

--- src/pages/Premium/PremiumFeaturedDialog.tsx
+++ src/pages/Premium/PremiumFeaturedDialog.tsx
@@ -1,10 +1,12 @@
 import { Tabs } from '@kobalte/core/tabs';
-import { Component, createEffect, createSignal, For, Show } from 'solid-js';
+import { Component, createEffect, createSignal, For, Match, Show, Switch } from 'solid-js';
 import AdvancedSearchDialog from '../../components/AdvancedSearch/AdvancedSearchDialog';
 import ButtonSecondary from '../../components/Buttons/ButtonSecondary';
 import HelpTip from '../../components/HelpTip/HelpTip';
 
 import styles from './Premium.module.scss';
+import { A, useNavigate } from '@solidjs/router';
+import ButtonLink from '../../components/Buttons/ButtonLink';
 
 export type FetureComparison = {
   name: string,
@@ -70,47 +72,85 @@ const premiumFeatures = [
     help: 'wss://premium.primal.net',
   },
   {
-    name: 'Nostr account backup',
+    name: 'Nostr contact list backup',
     free: 'false',
     premium: 'true',
-    help: 'Primal will backup 10 most recent versions of your profile, including your follow list. If your profile gets erased or corrupted by another nostr client, you will be able to restore it using Primal Nostr Tools. In addition, Primal will archive the complete history of all your posted notes, which you will be able to rebroadcast to your selected set of relays at any time.',
+    help: 'Primal creates a backup of 100+ most recent versions of your Nostr follow list. If your follow list gets erased or corrupted by another Nostr app, you will be able to restore it using the Contact List Backup tool in the Nostr Tools section for Primal Premium users.',
   },
   {
-    name: 'Premium feeds',
+    name: 'Nostr account content backup',
     free: 'false',
     premium: 'true',
-    help: 'Some advanced feeds will be accessible only to Primal Premium users. You will be able to access these feeds through other Nostr clients that support DVM feeds.',
+    help: 'Primal archives the complete history of all your Nostr content. You can rebroadcast any subset of your content to your selected set of relays at any time using the Content Backup tool in the Nostr Tools section for Primal Premium users.',
   },
+  // {
+  //   name: 'Premium feeds',
+  //   free: 'false',
+  //   premium: 'true',
+  //   help: 'Some advanced feeds will be accessible only to Primal Premium users. You will be able to access these feeds through other Nostr clients that support DVM feeds.',
+  // },
+  // {
+  //   name: 'Private beta access',
+  //   free: 'false',
+  //   premium: 'true',
+  //   help: 'Get early exclusive access to Primal Beta releases.',
+  // },
   {
-    name: 'Private beta access',
+    name: 'Much more to come!',
     free: 'false',
     premium: 'true',
-    help: 'Get early exclusive access to Primal Beta releases.',
-  },
-  {
-    name: 'More premium features to come!',
-    free: 'false',
-    premium: 'true',
-    help: 'We are working on a ton of new and exciting features for Primal Premium. Also feel free to reach out and let us know what you would like to see included.',
+    help: 'We are working on a ton of new and exciting features for Primal Premium. We will announce them as we get closer to releasing them. In the meantime, please feel free to reach out and let us know what you would like to see included in Primal Premium. All suggestions are welcome!',
   },
 ]
 
 const faq = [
   {
-    question: 'Become a Nostr power user and help shape the future?',
-    answer: 'At Primal, we don’t rely on advertising. We don’t monetize user data. Our users are our customers. Our sole focus is to make the best possible product for our users. We open source all our work to help the Nostr ecosystem flourish. By signing up for Primal Premium, you are enabling us to continue building for Nostr.',
+    question: 'How do I get support?',
+    answer: 'Simply email us at support@primal.net and include your Primal Name in the message. Support requests from Premium users are prioritized and typically handled on the same business day.',
   },
   {
-    question: 'Open protocols like Nostr give us the opportunity to regain control over our online lives?',
-    answer: 'At Primal, we don’t rely on advertising. We don’t monetize user data. Our users are our customers. Our sole focus is to make the best possible product for our users. We open source all our work to help the Nostr ecosystem flourish.',
+    question: 'Can I change my Primal Name?',
+    answer: 'Yes! If you wish to change your Primal Name, simply use the “Change your Primal Name” option in the Manage Premium section of any Primal app. Your new name will be functional immediately and your old name will be released and available to other users to register.',
   },
   {
-    question: 'Become a Nostr power user and help shape the future?',
-    answer: 'At Primal, we don’t rely on advertising. We don’t monetize user data. Our users are our customers. Our sole focus is to make the best possible product for our users. We open source all our work to help the Nostr ecosystem flourish. By signing up for Primal Premium, you are enabling us to continue building for Nostr.',
+    question: 'Do I have to use my Primal verified name and lightning address?',
+    answer: 'No. As a Primal Premium user you are able to reserve a Primal Name, but you are not required to use it as your nostr verified address (NIP-05), nor the bitcoin lightning address. Simply set any nostr verified address and/or the bitcoin lightning address you wish to use in your Nostr account profile settings.',
   },
   {
-    question: 'Open protocols like Nostr give us the opportunity to regain control over our online lives?',
-    answer: 'At Primal, we don’t rely on advertising. We don’t monetize user data. Our users are our customers. Our sole focus is to make the best possible product for our users. We open source all our work to help the Nostr ecosystem flourish.',
+    question: 'Do I own my Primal Name indefinitely?',
+    answer: 'You have the right to use your Primal Name for the duration of your Primal Premium subscription. After the subscription expires, there is a grace period of 30 days during which your Primal Name will not be available to others to register. Primal Legend users have non-expiring subscriptions, so they can use their Primal Names indefinitely. Please note that all Primal Names are owned by Primal and rented to users. Primal reserves the right to revoke any name if we determine that the name is trademarked by somebody else, that there is a possible case of impersonation, or for any other case of abuse, as determined by Primal. Please refer to our __LINK__Terms_of_Service__LINK__ for details.',
+  },
+  {
+    question: 'Can I buy multiple Primal Names?',
+    answer: 'We are working on adding the capability to manage multiple Primal Names. In the meantime, feel free to reach out to us via support@primal.net and we will try to accommodate.',
+  },
+  {
+    question: 'Is my payment information associated with my Nostr account?',
+    answer: 'No. Primal Premium can be purchased via an iOS App Store in-app purchase, Google Play in-app purchase, or directly over bitcoin lightning via the Primal web app. Regardless of the method of payment, your payment information is not associated with your Nostr account.',
+  },
+  {
+    question: 'Can I extend my subscription? How does that work?',
+    answer: 'Yes, you can extend your subscription using any of the payment methods we support: iOS App Store in-app purchase, Google Play in-app purchase, or directly over bitcoin lightning via the Primal web app. Any payment will extend your subscription by the number of months purchased. For example, if you purchase 3 Months of Primal Premium using the Primal web app, and then subscribe again via your mobile device, your subscription expiry date will be four months in the future, and it will continue to be pushed out with every subsequent monthly payment.',
+  },
+  {
+    question: 'If I buy Primal Premium on my phone, will I have access to it on the web?',
+    answer: 'Yes. Your Primal Premium subscription is assigned to your Nostr account. Therefore, regardless of the way you choose to subscribe, your Primal Premium subscription will be available to you in all Primal apps: web, iOS, Android.',
+  },
+  {
+    question: 'How does the Nostr contact list backup feature work?',
+    answer: 'Primal creates a backup of 100+ most recent versions of your Nostr follow list. If your follow list gets erased or corrupted by another Nostr app, you will be able to restore it using the Contact List Backup tool in the Nostr Tools section for Primal Premium users.',
+  },
+  {
+    question: 'How does the Nostr account content backup feature work?',
+    answer: 'Primal archives the complete history of all your Nostr content. You can rebroadcast any subset of your content to your selected set of relays at any time using the Content Backup tool in the Nostr Tools section for Primal Premium users.',
+  },
+  {
+    question: 'What other Premium features are coming in the future?',
+    answer: 'We are working on a ton of new and exciting features for Primal Premium. We will announce them as we get closer to releasing them. In the meantime, please feel free to reach out and let us know what you would like to see included in Primal Premium. All suggestions are welcome!',
+  },
+  {
+    question: 'I’d like to support Primal. Can I do more?',
+    answer: 'At Primal, we don’t rely on advertising. We don’t monetize user data. We open source all our work to help the Nostr ecosystem flourish. If you wish to help us continue doing this work, please see how you can __LINK__support_us__LINK__. Thank you from the entire Primal Team! 🙏❤️',
   },
 ]
 
@@ -118,6 +158,8 @@ const PremiumFeaturesDialog: Component<{
   open: 'features' | 'faq' | undefined,
   setOpen: (v: boolean) => void,
 }> = (props) => {
+
+  const navigate = useNavigate();
 
   const [activeTab, setActiveTab] = createSignal<'features' | 'faq'>('features');
 
@@ -137,6 +179,59 @@ const PremiumFeaturesDialog: Component<{
     if (featureTik === 'false') return <></>;
 
     return <>{featureTik}</>;
+  }
+
+  const parseAnswer = (text: string) => {
+    const tokens = text.split(' ');
+
+    const getLinkLabel = (t: string) => {
+      if (!t.startsWith('__LINK__')) return ['', ''];
+      const s = t.split('__LINK__');
+
+      const label = s[1].replaceAll('_', ' ');
+      const suffix = s[2] || '';
+
+
+      return [label, suffix]
+    }
+
+    const goTo = (label: string) => {
+
+      if (label === 'support us') {
+        props.setOpen(false);
+        navigate('/premium/support');
+        return;
+      }
+
+      if (label === 'Terms of Service') {
+        // @ts-ignore
+        window.open(`${location.origin}/terms`, '_blank').focus();
+        return;
+      }
+
+      props.setOpen(false);
+      navigate('/home');
+    }
+
+    return (<>
+      <For each={tokens}>
+        {token => {
+          const [label, suffix] = getLinkLabel(token);
+
+          return (
+          <Switch
+            fallback={<>{token}&nbsp;</>}
+          >
+            <Match when={token.startsWith('__LINK__')}>
+              <>
+                <ButtonLink onClick={() => goTo(label)}>{label}</ButtonLink>
+                <>{suffix}</>&nbsp;
+              </>
+            </Match>
+          </Switch>
+        )}}
+      </For>
+    </>)
   }
 
   return (
@@ -163,6 +258,15 @@ const PremiumFeaturesDialog: Component<{
         <Tabs value={activeTab()} >
           <Tabs.Content value="features" >
             <div class={styles.tabContentPremiumFeatures}>
+              <div class={styles.whyPremium}>
+                <div class={styles.caption}>Why Premium</div>
+                <div class={styles.description}>
+                  Become a Nostr power user and help shape the future! Open protocols like Nostr give us the opportunity to regain control over our online lives.
+                  At Primal, we don’t rely on advertising. We don’t monetize user data. Our users are our customers. Our sole focus is to make the best possible product for our users. We open source all our work to help the Nostr ecosystem flourish. By signing up for Primal Premium, you are enabling us to continue building for Nostr.
+                  Be the change you want to see in the world. If you don’t want to be the product, consider being the customer.
+                </div>
+              </div>
+
               <table>
                 <thead>
                   <tr>
@@ -195,19 +299,19 @@ const PremiumFeaturesDialog: Component<{
           </Tabs.Content>
           <Tabs.Content value="faq" >
             <div class={styles.tabContentPremiumFeatures}>
-              <For each={faq}>
-                {q => (
-                  <div class={styles.faq}>
-                    <div class={styles.question}>
-                      {q.question}
+                <For each={faq}>
+                  {q => (
+                    <div class={styles.faq}>
+                      <div class={styles.question}>
+                        {q.question}
+                      </div>
+                      <div class={styles.answer}>
+                        {parseAnswer(q.answer)}
+                      </div>
                     </div>
-                    <div class={styles.answer}>
-                      {q.answer}
-                    </div>
-                  </div>
-                )}
-              </For>
-            </div>
+                  )}
+                </For>
+              </div>
           </Tabs.Content>
         </Tabs>
 
