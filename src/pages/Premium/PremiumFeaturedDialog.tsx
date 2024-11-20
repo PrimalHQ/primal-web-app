5c9f77dd0c89fc8ca57ea32e08cd1ea4401bacaf
Bojan Mojsilovic <bmojsilovic@itekako.com>
2024-11-20 11:58:24 +0100

    Premium: Remove Why Premium?

--- src/pages/Premium/PremiumFeaturedDialog.tsx
+++ src/pages/Premium/PremiumFeaturedDialog.tsx
@@ -235,15 +235,6 @@ const PremiumFeaturesDialog: Component<{
         <Tabs value={activeTab()} >
           <Tabs.Content value="features" >
             <div class={styles.tabContentPremiumFeatures}>
-              <div class={styles.whyPremium}>
-                <div class={styles.caption}>Why Premium</div>
-                <div class={styles.description}>
-                  Become a Nostr power user and help shape the future! Open protocols like Nostr give us the opportunity to regain control over our online lives.
-                  At Primal, we don’t rely on advertising. We don’t monetize user data. Our users are our customers. Our sole focus is to make the best possible product for our users. We open source all our work to help the Nostr ecosystem flourish. By signing up for Primal Premium, you are enabling us to continue building for Nostr.
-                  Be the change you want to see in the world. If you don’t want to be the product, consider being the customer.
-                </div>
-              </div>
-
               <table>
                 <thead>
                   <tr>
