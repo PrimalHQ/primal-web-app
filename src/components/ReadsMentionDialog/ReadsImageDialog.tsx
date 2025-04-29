61f1e8d2f07f04ab6d2f0dec0935004bdc5e3826
Bojan Mojsilovic <bmojsilovic@itekako.com>
2025-04-29 17:01:01 +0200

    Attach media upload styling

--- src/components/ReadsMentionDialog/ReadsImageDialog.tsx
+++ src/components/ReadsMentionDialog/ReadsImageDialog.tsx
@@ -42,6 +42,7 @@ import ButtonSecondary from '../Buttons/ButtonSecondary';
 import Uploader from '../Uploader/Uploader';
 import { useToastContext } from '../Toaster/Toaster';
 import UploaderBlossom from '../Uploader/UploaderBlossom';
+import { Progress } from '@kobalte/core/progress';
 
 
 const ReadsImageDialog: Component<{
@@ -63,7 +64,11 @@ const ReadsImageDialog: Component<{
     alt: '',
     title: '',
     image: '',
-  })
+  });
+
+  const [isUploading, setIsUploading] = createSignal(false);
+  const [cancelUploading, setCancelUploading] = createSignal<() => void>();
+  const [imageLoaded, setImageLoaded] = createSignal(false);
 
   createEffect(() => {
     const e = props.editor;
@@ -89,6 +94,7 @@ const ReadsImageDialog: Component<{
 
   const resetUpload = () => {
     setFileToUpload(undefined);
+    setIsUploading(false);
   };
 
   const onUploadContent = (file: File) => {
@@ -119,60 +125,137 @@ const ReadsImageDialog: Component<{
             class={styles.uploadHolder}
             onClick={() => contentFileUpload?.click()}
           >
-            <Show
-              when={state.image.length > 0}
-              fallback={
-                <div class={styles.uploadPlaceholder}>
-                  <div class={styles.attachIcon}></div>
-                  <div class={styles.attachLabel}>
-                    upload image
+
+            <Switch>
+              <Match
+                when={isUploading()}
+              >
+                <div
+                  class={styles.uploadingOverlay}
+                  onClick={() => {
+                    const canc = cancelUploading();
+                    resetUpload();
+                    canc && canc();
+                  }}
+                >
+                  <div>Cancel Upload</div>
+                  <div class={styles.closeBtn}>
+                    <div class={styles.closeIcon}></div>
                   </div>
                 </div>
-              }
-            >
-              <img src={state.image} />
-            </Show>
+              </Match>
+
+              <Match
+                when={state.image.length > 0}
+              >
+                <div
+                  class={styles.uploadButton}
+                >
+                  <Show when={imageLoaded()}>
+                    <div
+                      class={styles.uploadOverlay}
+                      onClick={() => {
+                        document.getElementById('upload-title-image')?.click();
+                      }}
+                    >
+                      <div
+                        class={styles.closeBtn}
+                        onClick={(e: MouseEvent) => {
+                          e.preventDefault();
+                          e.stopPropagation();
+                          setState(() => ({ image: '' }));
+                          setImageLoaded(false);
+                          document.getElementById('upload-title-image')?.click();
+                        }}
+                      >
+                        <div class={styles.closeIcon}></div>
+                      </div>
+                      <div>Chage hero Image</div>
+                    </div>
+                  </Show>
+                  <img
+                    class={styles.titleImage}
+                    src={state.image}
+                    onload={() => setImageLoaded(true)}
+                  />
+                </div>
+              </Match>
+
+              <Match
+                when={state.image.length === 0}
+              >
+                <div class={styles.noTitleImagePlaceholder}>
+                  <div class={styles.uploadPlaceholder}>
+                    <div class={styles.attachIcon}></div>
+                    <div class={styles.attachLabel}>
+                      upload image
+                    </div>
+                  </div>
+                </div>
+              </Match>
+            </Switch>
 
             <input
-              id="upload-content"
+              id="upload-new-media"
               type="file"
               onChange={uploadFile}
               ref={contentFileUpload}
               hidden={true}
               accept="image/*,video/*,audio/*"
             />
+
+
+            <UploaderBlossom
+              uploadId="upload_content_image"
+              hideLabel={false}
+              publicKey={account?.publicKey}
+              nip05={account?.activeUser?.nip05}
+              file={fileToUpload()}
+              onFail={() => {
+                toast?.sendWarning(intl.formatMessage(tUpload.fail, {
+                  file: fileToUpload()?.name,
+                }));
+                resetUpload();
+              }}
+              onRefuse={(reason: string) => {
+                if (reason === 'file_too_big_100') {
+                  toast?.sendWarning(intl.formatMessage(tUpload.fileTooBigRegular));
+                }
+                if (reason === 'file_too_big_1024') {
+                  toast?.sendWarning(intl.formatMessage(tUpload.fileTooBigPremium));
+                }
+                resetUpload();
+              }}
+              onCancel={() => {
+                resetUpload();
+              }}
+              onSuccsess={(url:string) => {
+                setState(() => ({ image: url }))
+
+                resetUpload();
+              }}
+              onStart={(_, cancelUpload) => {
+                setIsUploading(true);
+                setImageLoaded(false);
+                setCancelUploading(() => cancelUpload);
+              }}
+              progressBar={(uploadState, resetUploadState) => {
+
+                return (
+                  <Progress value={uploadState.progress} class={styles.uploadProgress}>
+                    <div class={styles.progressTrackContainer}>
+                      <Progress.Track class={styles.progressTrack}>
+                        <Progress.Fill
+                          class={`${styles.progressFill}`}
+                        />
+                      </Progress.Track>
+                    </div>
+                  </Progress>
+                );
+              }}
+            />
+
           </div>
-
-          <UploaderBlossom
-            uploadId="upload_content_image"
-            hideLabel={false}
-            publicKey={account?.publicKey}
-            nip05={account?.activeUser?.nip05}
-            file={fileToUpload()}
-            onFail={() => {
-              toast?.sendWarning(intl.formatMessage(tUpload.fail, {
-                file: fileToUpload()?.name,
-              }));
-              resetUpload();
-            }}
-            onRefuse={(reason: string) => {
-              if (reason === 'file_too_big_100') {
-                toast?.sendWarning(intl.formatMessage(tUpload.fileTooBigRegular));
-              }
-              if (reason === 'file_too_big_1024') {
-                toast?.sendWarning(intl.formatMessage(tUpload.fileTooBigPremium));
-              }
-              resetUpload();
-            }}
-            onCancel={() => {
-              resetUpload();
-            }}
-            onSuccsess={(url:string) => {
-              setState(() => ({ image: url }))
-
-              resetUpload();
-            }}
-          />
         </div>
         <div class={styles.inputHolder}>
           <label for="input_title">Image title:</label>
